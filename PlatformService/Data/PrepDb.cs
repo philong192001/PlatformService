ee6d8cea8be767d5f60b607d015f954055fd6777
Phí Long <longpv21.it@gmail.com>
2023-07-12 08:54:02 +0700

    PlatformService

---
+++ PlatformService/Data/PrepDb.cs
@@ -0,0 +1,34 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data;
+
+public static class PrepDb
+{
+    public static void PrepPopulation(IApplicationBuilder application)
+    {
+        using( var serviceScope = application.ApplicationServices.CreateScope())
+        {
+            SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+        }
+    }
+
+    private static void SeedData(AppDbContext context)
+    {
+        if (!context.Platforms.Any())
+        {
+            Console.WriteLine(" ->> Seeding data");
+
+            context.Platforms.AddRange(
+                new Platform() { Name = " Dot Net", Publisher = "Microsoft", Cost = " Free" },
+                new Platform() { Name = " Dot Net Core", Publisher = "GoJek", Cost = " Free" },
+                new Platform() { Name = " K8S", Publisher = "Bee", Cost = " Free" },
+                new Platform() { Name = " SQL", Publisher = "Alibaba", Cost = " Free" }
+                );
+            context.SaveChanges();
+        }
+        else
+        {
+            Console.WriteLine(" ->> We already have data");
+        }
+    }
+}
